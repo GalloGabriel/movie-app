d7e7d1160e2845825d45eff541fa1eb05dcffcd8
GalloGabriel <gabriel.gallo2242@gmail.com>
2021-07-29 01:44:16 -0300

    arrumando parte do bug em edição da avaliação

--- src/pages/Assistidos/index.js
+++ src/pages/Assistidos/index.js
@@ -38,6 +38,7 @@ export default function Assistidos(){
     let filtroFilmes = filmes.filter((item)=>{
       return (item.id !== id)
     })
+    
 
     //salvando os filmes na pagina sem o filme deletado
     setFilmes(filtroFilmes);
@@ -46,9 +47,9 @@ export default function Assistidos(){
 
   }
 
-
+  
   function toggleModal(){
-    let modal = document.getElementById('myModal');
+    let modal = document.getElementById('myModal-1');
     let span = document.getElementsByClassName('close')[0];
     let body = document.getElementsByClassName('app')[0];
     let range = document.getElementById('range');
@@ -59,7 +60,7 @@ export default function Assistidos(){
     let label03 = document.getElementById('label03');
 
     modal.style.display = "block";
-    $("#myModal").animate({top: "30%", opacity: "1"},1000);
+    $("#myModal-1").animate({top: "30%", opacity: "1"},1000);
 
     span.onclick = function(){
       modal.style.display = "none";
@@ -86,7 +87,7 @@ export default function Assistidos(){
       <span className="zeroFilmes">Você ainda não possui nenhum filme salvo :(</span> }
 
       <ul>
-        {filmes.map((item)=>{
+        {filmes.map((item, index)=>{
           return(
             <li key={item.id}>              
               <span className="titulo-assistidos">{item.title}</span><br/>
@@ -123,13 +124,15 @@ export default function Assistidos(){
               <br/>  
               <div style={{marginTop: '-25px'}}>
               <Link className="details" to={`/filme/${item.id}`}>Ver Detalhes</Link>  
-              <button className="edit-button" id="toggleModal" onClick={ toggleModal }>Editar</button>          
+              <button className="edit-button" id="toggleModal" onClick={() => toggleModal }>Editar</button>          
               <button className="button-assistidos" onClick={ () => handleDelete(item.id) }>Remover</button>
               </div>
 
-              <div id="myModal" className="modal-assistidos">
+
+              <div id={`myModal-${index+1}`} className="modal-assistidos">
+                {item.id}
                 <span className="close">&times;</span>
-                <h1 id="titulo">Avalie este filme!</h1>
+                <h1 id="titulo">Edite sua avaliação</h1>
 
                 <label id="label01">Nota do Filme:</label><br/>
                 <input id="range" type="range" min="0" max="10" step="0.5" value={range}
@@ -153,9 +156,9 @@ export default function Assistidos(){
 
                 <br/>
 
-          <button className="btnModal" onClick={() => editaFilme(item.id) }>Salvar Filme</button>
+                <button className="btnModal" onClick={() => editaFilme(item.id) }>Salvar Filme</button>
           
-        </div>
+              </div>
   
             </li>
           )
